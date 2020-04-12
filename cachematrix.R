29b6bebf7ce4a62c2809c81eca38d78aeb631b96
Mingshuo Zeng <zengmingshuo@gmail.com>
2020-04-11 19:24:57 -0700

    Commit from RStudio

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setimatrix <- function(source) s
+  getimatrix <- function() s
+  list(set = set, get = get,
+       setimatrix = setimatrix,
+       getimatrix = getimatrix)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+  s <- x$getimatrix()
+  if(!is.null(s)){
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setimatrix(s)
+  s
         ## Return a matrix that is the inverse of 'x'
 }
